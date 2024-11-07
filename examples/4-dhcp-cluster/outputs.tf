58b0cf7037b280309af111c20b908b8cf1922a27
Vincent Dupain <vincent.dupain@protonmail.com>
2024-11-07 09:06:29 +0000

    feat: add dhcp cluster example

---
+++ examples/4-dhcp-cluster/outputs.tf
@@ -0,0 +1,13 @@
+output "cluster_name" {
+  value     = module.talos-k8s-cluster.cluster_name
+  sensitive = false
+}
+
+output "config_ipv4_addresses" {
+  value = module.talos-k8s-cluster.config_ipv4_addresses
+}
+
+
+output "qemu_ipv4_addresses" {
+  value = module.talos-k8s-cluster.qemu_ipv4_addresses
+}
