bab78580e92baff7c91d5c7aa2fd9c9b87c1656b
ⓥⓘⓝⓒⓔ⑨ⓟⓐⓡⓐ <vince9p+github@pm.me>
2025-02-28 09:21:39 +0100

    Some fixes (#6)
    * feat: network device bridge config variable
    
    * feat: disk file format config variable
    
    ---------
    
    Co-authored-by: Vincent Dupain <vincent.dupain@protonmail.com>

--- modules/vms_proxmox/variables.tf
+++ modules/vms_proxmox/variables.tf
@@ -15,27 +15,29 @@ variable "proxmox" {
 variable "cluster" {
   description = "Cluster configuration"
   type = object({
-    network_dhcp  = optional(bool, false)
-    gateway       = string
-    cidr          = number
-    vlan_id       = optional(number, null)
-    name          = string
-    talos_version = optional(string, "v1.9.0")
+    network_dhcp          = optional(bool, false)
+    gateway               = string
+    cidr                  = number
+    vlan_id               = optional(number, null)
+    network_device_bridge = optional(string, "vmbr0")
+    name                  = string
+    talos_version         = optional(string, "v1.9.4")
   })
 }
 
 variable "vms" {
   description = "Configuration for cluster nodes"
   type = map(object({
-    host_node      = string
-    machine_type   = string
-    datastore_id   = optional(string, "local-lvm")
-    ip             = string
-    cpu            = number
-    ram_dedicated  = number
-    os_disk_size   = number
-    data_disk_size = number
-    gpu            = optional(string)
+    host_node        = string
+    machine_type     = string
+    datastore_id     = optional(string, "local-lvm")
+    ip               = string
+    cpu              = number
+    ram_dedicated    = number
+    os_disk_size     = number
+    data_disk_size   = number
+    disk_file_format = optional(string, "raw")
+    gpu              = optional(string)
   }))
 }
 
